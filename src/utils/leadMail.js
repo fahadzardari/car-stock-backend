451604e63feace39ae4592d490092b10ef115d2d
fahad <fahadzardari111@gmail.com>
2023-10-06 15:25:36 +0500

    updated mail and onDelete cascade lead

--- src/utils/leadMail.js
+++ src/utils/leadMail.js
@@ -4,16 +4,43 @@ import { transporter } from "../config/mail.js";
 export const sendMail = (lead) => {
   const mailOptions = {
     from: MAIL,
-    to: "fahadzardari222@gmail.com",
+    to: "Grace.int01@gmail.com",
     subject: "Lead Is Created",
-    html: `<h1>Lead is created</h1>
-           <p>Lead Name: ${lead.name}</p>
-           <p>Lead Email: ${lead.email}</p>
-           <p>Lead Phone: ${lead.phone}</p>
-           <p>Interested Make: ${lead.make}}</p>
-           <p>Interested Model: ${lead.model}</p>
-           <p>Interested CarId: ${lead.carId}</p>
-           <p>Message: ${lead.message}</p>
+    html: `
+      <html>
+        <head>
+          <style>
+            body {
+              font-family: Arial, sans-serif;
+              background-color: #f4f4f4;
+            }
+            .container {
+              background-color: #fff;
+              padding: 20px;
+              border-radius: 5px;
+              box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            }
+            h1 {
+              color: #333;
+            }
+            p {
+              margin: 5px 0;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <h1>Lead is created</h1>
+            <p><strong>Lead Name:</strong> ${lead.name}</p>
+            <p><strong>Lead Email:</strong> ${lead.email}</p>
+            <p><strong>Lead Phone:</strong> ${lead.phone}</p>
+            <p><strong>Interested Make:</strong> ${lead.make}</p>
+            <p><strong>Interested Model:</strong> ${lead.model}</p>
+            <p><strong>Interested CarId:</strong> ${lead.carId}</p>
+            <p><strong>Message:</strong> ${lead.message}</p>
+          </div>
+        </body>
+      </html>
     `,
   };
   transporter.sendMail(mailOptions, function (error, info) {
@@ -25,3 +52,4 @@ export const sendMail = (lead) => {
     }
   });
 };
+
