19c8719821b971584b0b50e4afbb365c1062fd8a
fahad <fahadzardari111@gmail.com>
2023-10-07 12:45:06 +0500

    added gracejapan mail server

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,11 +1,12 @@
 import nodemailer from "nodemailer";
 import { MAIL, MAIL_PASSWORD } from "./envVariables.js";
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  //service: "gmail",
+  host: "mail.grace-japan.com",
+  port: 465,
+  secure: true,
   auth: {
     user: MAIL,
     pass: MAIL_PASSWORD,
   },
 });
-
-
